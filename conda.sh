38895c0052aee096eb8fde9eb2d8347ae50e7a5a
CHJPC\liker <likercho63@gmail.com>
2025-10-24 21:00:51 +0900

    .

--- conda.sh
+++ conda.sh
@@ -6,6 +6,8 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 bash Miniconda3-latest-Linux-x86_64.sh -b
 
+cd ..
+
 source miniconda3/bin/activate
 
 echo "Success to build anaconda environment. Please restart the terminal."
